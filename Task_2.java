643d9ffd6dc09f18c4155ee8773971926a6e8738
pleewson <adrianplewait@gmail.com>
2022-06-13 12:32:59 +0200

    started tables

---
+++ Task_2.java
@@ -0,0 +1,8 @@
+public class Task_2 {
+    public static void main (String[] args){
+        String[] fruits = {"apple", "banana", "berry"};
+
+        System.out.println( fruits[0] );
+        System.out.println( fruits.length);
+    }
+}
---
+++ Task_3.java
@@ -0,0 +1,10 @@
+public class Task_3 {
+    public static void main(String[] args) {
+        int[] numbers = {4, 643, 112, 9999, 69};
+        int sum;
+
+        for (sum = 0; sum < 5; sum++) {
+            numbers[sum]
+        }
+    }
+}
