7802231d8fb66417fbc15c209ec430332dfe6987
pleewson <adrianplewait@gmail.com>
2022-06-13 13:32:23 +0200

    variables ready

---
+++ Zad2.java
@@ -0,0 +1,11 @@
+public class Zad2 {
+    public static void main(String[] args){
+        byte nr1 = 94;
+        short nr2 = 257;
+        int result;
+
+        result = nr1+nr2;
+
+        System.out.print(result);
+    }
+}
---
+++ Zad5.java
@@ -0,0 +1,12 @@
+public class Zad5 {
+    public static void main(String[] args){
+
+        double nr1 = 5.1;
+        float nr2 = 5.0f;
+
+        boolean result;
+        result = nr1 > nr2;
+
+        System.out.println(result);
+    }
+}
