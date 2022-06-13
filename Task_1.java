0596ee9cb7ccad80ee5fcff053cfddecb01e48dc
pleewson <adrianplewait@gmail.com>
2022-06-13 12:36:29 +0200

    nic

--- Task_1.java
+++ Task_1.java
@@ -1,9 +1,3 @@
 public class Task_1 {
-    public static void main(String[] args){
-
-        int [] threeElements = {3, 4, 5};
-        System.out.print(threeElements[0] + " ");
-        System.out.print(threeElements[1] + " ");
-        System.out.print(threeElements[2] + " ");
-    }
+    
 }
