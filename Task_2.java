af6758654692988c2e0d9f5d5a7230b7ee56e297
pleewson <adrianplewait@gmail.com>
2022-06-13 22:13:57 +0200

    a bit ruszone

--- Task_2.java
+++ Task_2.java
@@ -2,7 +2,9 @@ public class Task_2 {
     public static void main (String[] args){
         String[] fruits = {"apple", "banana", "berry"};
 
+        System.out.println("ilość liter pierwszego słowa to " + fruits[0].length());
+
         System.out.println( fruits[0] );
-        System.out.println( fruits.length);
+        
     }
 }
--- Task_3.java
+++ Task_3.java
@@ -1,10 +1,12 @@
 public class Task_3 {
     public static void main(String[] args) {
         int[] numbers = {4, 643, 112, 9999, 69};
-        int sum;
+        int sum = 0;
 
-        for (sum = 0; sum < 5; sum++) {
-            numbers[sum]
+        for (int i = 1; i < 5; i++) {
+           sum += numbers[i]; // sum = 0+4, 0+643 itd
         }
+        System.out.println("SUMA: " + sum);
+
     }
 }
