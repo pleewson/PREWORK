1ebb66cd2820f9542ee495a560aecda302c1e260
pleewson <adrianplewait@gmail.com>
2022-06-14 22:30:22 +0200

    nicniezorbilem

--- Task_4.java
+++ Task_4.java
@@ -1,19 +1,16 @@
 public class Task_4 {
     public static void main(String[] args){
+
         int[] numbers = {4, 643, 112, 9999, 69};
 
         for( int i = 0; i <5 ; i++){
             if( numbers[i] % 2 == 0){
-                System.out.println(i + " - parity");
+                System.out.println(numbers + " - parity");
             }
         }
 
        // System.out.println(numbers[1]);
     }
-        {
-
-            }
-
         }
 
 
