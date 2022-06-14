8df9d77f8ba89a1612649679ecc490542ffea08c
pleewson <adrianplewait@gmail.com>
2022-06-14 23:05:17 +0200

    task 4 complete

--- Task_4.java
+++ Task_4.java
@@ -1,16 +1,23 @@
 public class Task_4 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         int[] numbers = {4, 643, 112, 9999, 69};
 
-        for( int i = 0; i <5 ; i++){
-            if( numbers[i] % 2 == 0){
-                System.out.println(numbers + " - parity");
+        {    int sum0dd = 0;
+
+        for (int i = 0; i < 5; i++) {
+            if (numbers[i] % 2 == 0) {
+                System.out.println(numbers[i] + " - parity");
+            } else {
+                sum0dd += numbers[i];
+                //  System.out.println(numbers[sum0dd]);
             }
         }
-
-       // System.out.println(numbers[1]);
-    }
+            System.out.println("SUMA: " + sum0dd);
         }
+    }
+
+            // System.out.println(numbers[1]);
+}
 
 
