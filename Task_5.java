b0b583b268ec8bb5cbc09629c08e10e94c5207c5
pleewson <adrianplewait@gmail.com>
2022-06-15 00:00:53 +0200

    complete tasks

---
+++ Task_5.java
@@ -0,0 +1,16 @@
+public class Task_5 {
+    public static void main(String[] args){
+
+        int[] numbers = {4, 643, 112, 9999, -69};
+        int min = 0;
+
+        for( int i = 0; i < 6; i++){
+            if ( min > numbers[i]){
+                min = numbers[i];
+// CO BY BYLO LEPSZE OD 0 ???
+                System.out.println("MIN: " + min);
+            }
+        }
+
+    }
+}
---
+++ Task_6.java
@@ -0,0 +1,9 @@
+public class Task_6 {
+    public static void main(String[] args){
+
+        int [] numbers = {4, 643, 112, 9999, -69};
+        for ( int i = 4; i > -1; i--){
+            System.out.println(numbers[i]);
+        }
+    }
+}
