379edc94a1052c70cdb2ecaf7bd3b4a9bee316cd
pleewson <adrianplewait@gmail.com>
2022-06-15 23:53:11 +0200

    task 2 close to end

--- Task_2.java
+++ Task_2.java
@@ -2,9 +2,20 @@ public class Task_2 {
     public static void main (String[] args){
         String[] fruits = {"apple", "banana", "berry"};
 
-        System.out.println("ilość liter pierwszego słowa to " + fruits[0].length());
+       /* int [] papaje = {1,3,4,5,6,7};
+        System.out.println(papaje[papaje.length-3]);
+        */
 
-        System.out.println( fruits[0] );
+        System.out.println(fruits[0]);
+
+        System.out.println(fruits[ fruits.length-1 ]);
+
+        //System.out.println(fruits[fruits.length-3] + fruits[fruits.length-2] + fruits[fruits.length-1]);
+
+        
+
+
+
         
     }
 }
