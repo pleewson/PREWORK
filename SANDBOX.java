fd0b75814604b3e4e7b67a6782b17c289cc3f5c3
pleewson <adrianplewait@gmail.com>
2022-06-16 21:05:06 +0200

    i did it lel

--- SANDBOX.java
+++ SANDBOX.java
@@ -2,13 +2,11 @@ public class SANDBOX {
     public static void main(String[] args){
 
         int[] numbers = {5,10,15,20};
-        int suma;
+        int suma = 0;
 
         for (int i = 0; i < 4; i++){
-            suma = numbers[i] += 0; // 5 + 0,
-            System.out.println(suma);
-
+            suma += numbers[i];
         }
-       // System.out.println(suma);
+        System.out.println(suma);
     }
 }
--- Task_7.java
+++ Task_7.java
@@ -3,19 +3,30 @@ import java.util.Locale;
 public class Task_7 {
     public static void main(String[] args){
             double[] tempCelc = {30, 29, 14, 42, -4, -10, 8, 14, 32, 11, 8, 0, 0};
-            double tempFaren;
+            double[] tempFaren = new double[13]; //{0,0,0,0,0,0,0,0,0,0,0,0,0};
+
+            double suma = 0;
+
+            String suma00;
+            String tempFaren00;
 
 
-            for (int i = 0; i < 13; i++){
-                tempFaren = tempCelc[i] * 1.8 + 32;
-             //   Locale l = Locale.ENGLISH;
-              //  String tempFaren00 = String.format(l, "%.2f", tempFaren);
-                System.out.println(tempFaren);
-                    for (int j = 0; j < 13; j++){
+        for (int i = 0; i < 13; i++){
+                tempFaren[i] = tempCelc[i] * 1.8 + 32;
+                tempFaren00 = String.format(Locale.ENGLISH, "%.2f", tempFaren[i]);
 
-                }
+                System.out.println(tempCelc[i] +" tempCelc -> tempFaren = " + tempFaren00);
+                suma += tempFaren[i];
             }
-    }
+        System.out.println();
 
+           suma /= tempFaren.length;
+
+           suma00 = String.format(Locale.ENGLISH, "%.2f", suma);
+        System.out.println("ŚREDNIA: " + suma00);
+
+    }
 }
+
+
 
