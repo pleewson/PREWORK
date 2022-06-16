8e3626744e0813b2bc141e001d47698a75905930
pleewson <adrianplewait@gmail.com>
2022-06-16 20:22:08 +0200

    troche zrobione

--- SANDBOX.java
+++ SANDBOX.java
@@ -1,2 +1,14 @@
 public class SANDBOX {
+    public static void main(String[] args){
+
+        int[] numbers = {5,10,15,20};
+        int suma;
+
+        for (int i = 0; i < 4; i++){
+            suma = numbers[i] += 0; // 5 + 0,
+            System.out.println(suma);
+
+        }
+       // System.out.println(suma);
+    }
 }
---
+++ Task_7.java
@@ -0,0 +1,21 @@
+import java.util.Locale;
+
+public class Task_7 {
+    public static void main(String[] args){
+            double[] tempCelc = {30, 29, 14, 42, -4, -10, 8, 14, 32, 11, 8, 0, 0};
+            double tempFaren;
+
+
+            for (int i = 0; i < 13; i++){
+                tempFaren = tempCelc[i] * 1.8 + 32;
+             //   Locale l = Locale.ENGLISH;
+              //  String tempFaren00 = String.format(l, "%.2f", tempFaren);
+                System.out.println(tempFaren);
+                    for (int j = 0; j < 13; j++){
+
+                }
+            }
+    }
+
+}
+
