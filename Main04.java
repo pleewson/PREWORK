cc9899d91d5bcf522f495c3968213052717e937b
pleewson <adrianplewait@gmail.com>
2022-06-20 20:46:09 +0200

    Main 06 complete

--- Main04.java
+++ Main04.java
@@ -20,15 +20,37 @@ public class Main04 {
             i++;
         }
 
+        System.out.println();
 
 
         int[] numbers = {4, 643, 112, 9999, 69};
-        int sum = 0;
+        int sum0dd = 0;
 
         for(int j = 0; j < numbers.length; j++){
-            if(numbers[j] %= 2){
+            if(numbers[j] %2 == 0){
                 System.out.println(numbers[j]);
+            }else{
+                sum0dd = sum0dd += numbers[j];
             }
+        }
+        System.out.println("SUMA: " + sum0dd);
+
+
+
+
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+        int c = Integer.parseInt(args[2]);
+
+        if((a+b)>c){
+            if((c+b)>a){
+                if((a+c>b)){
+                    System.out.println("YES");
+                }
+            }
+        }else{
+            System.out.println("NO");
+
         }
 
     }
