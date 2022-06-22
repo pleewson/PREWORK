2969e1c375d35f889a33cc4029b08d462598847c
pleewson <adrianplewait@gmail.com>
2022-06-22 23:54:54 +0200

    updates

--- Main06.java
+++ Main06.java
@@ -1,12 +1,32 @@
 public class Main06 {
     public static void main(String[] args){
 
-        int num = 9;
+       /** int num = 3;
 
         for (int i = 1; i <= num; i++){
             //System.out.printf("%d * %d = %d \n", num, i, num * i);
-            System.out.println(num + " * " + i + " = " + num*i);
+           // System.out.println(num + " * " + i + " = " + num*i);
+
+        }
+        */
+
+        //TABLICZKA MNOŻENIA
+       /** int value = 3;
+
+        for(int i = 1; i<11; i++){
+            System.out.printf("%d * %d = %d \n",value, i, i * value); // 1 * 2,  2 * 2
+        */
+
+
+       int num = 3;
+       for(int i = 1; i <= num; i++){
+           //for(int j = 1; j <= num; j++)
+           
+           System.out.printf("%d * %d = %d \n", i, i, i * i);
+
+       }
+
         }
 
     }
-}
+
