218fa2ee24d6a953f72dd22569456619b1df2905
pleewson <adrianplewait@gmail.com>
2022-06-24 00:53:37 +0200

    finished:)

--- Main07.java
+++ Main07.java
@@ -1,22 +1,22 @@
 public class Main07 {
     public static void main(String[] args) {
 
-        int paramStart = 3;
+        int paramStart = Integer.parseInt(args[0]);
+        int x;
+        int y;
 
-        for (int x = 1; x <= 2; x++) {
-            for (int y = x; y <= paramStart; y++) {
 
-                System.out.printf("%d", paramStart, x * y);
-
+        for (x = 1; x < 2; x++) {
+             System.out.printf("%d! = ", paramStart);
+            for (y = x; y <= paramStart; y++){
+                x *= y;
             }
-
-
-
-            //System.out.printf("%d! = %d", paramStart, x * (x=+1));
-            }
+            System.out.print(x);
 
         }
+
     }
+}
 
 
 
