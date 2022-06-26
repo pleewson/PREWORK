56c1528046595f0c96e0d47f1c961b00ff46580e
pleewson <adrianplewait@gmail.com>
2022-06-26 15:17:29 +0200

    start main08

---
+++ Main08.java
@@ -0,0 +1,17 @@
+public class Main08 {
+    public static void main(String[] args) {
+
+        int a = 420;
+        int b = 168;
+
+       // int x = 5, y = 6, z = 50;
+
+
+
+        /*int c = (a > b) ? a % b : b % a;
+        if (c == 0) return (a > b) ? b : a;
+        return (a > b) ?
+
+         */
+    }
+}
