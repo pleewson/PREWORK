c30924277ec0135f5995eeae54dfca2a52c99a27
pleewson <adrianplewait@gmail.com>
2022-07-11 10:23:24 +0200

    Imieniny 100%

---
+++ Imieniny.java
@@ -0,0 +1,25 @@
+public class Imieniny {
+    /**  https://pl.spoj.com/problems/MWPZ06D/  */
+
+    public static void main(String[] args){
+
+        int dataInformation; // D (1 ≤ D ≤ 500)
+        // L i C (1 ≤ L, C ≤ 109) oznaczających odpowiednio liczbę osób
+        // w klasie Jasia oraz liczbę cukierków, które kupiła mama.
+
+
+        int amoutPeople =  Integer.parseInt(args[0]);
+        int amoutsSweets = Integer.parseInt(args[1]);
+
+
+        if(amoutPeople < amoutsSweets) {
+            System.out.println("BON APETIT people" + (args[0]) + " - sweets" + (args[1]));
+        }else{
+            System.out.println("NOT ENOUGH SWEETS people" + (args[0]) + " - sweets" + (args[1]));
+
+        }
+
+
+
+    }
+}
